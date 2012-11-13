76ffc9a47d8db74dcd008cc0f09c044f041eced7
Jarred Olson <jarred.olson@iq-ity.com>
2012-11-13 10:24:29 -0500

    Able to parse wikipedia page

--- test/integration/jsoup/JsoupTests.groovy
+++ test/integration/jsoup/JsoupTests.groovy
@@ -8,13 +8,36 @@ import de.l3s.boilerpipe.extractors.ArticleSentencesExtractor
 
 class JsoupTests {
 
-    def url = "http://www.10tv.com/content/stories/2012/11/09/columbus-high-speed-chase-overnight.html"
+    @Test
+    public void newsArticle() {
+        def url = "http://www.10tv.com/content/stories/2012/11/09/columbus-high-speed-chase-overnight.html"
+
+        def result = doIt(url)
+
+        assert result.startsWith('<div id="story-body">')
+    }
 
     @Test
-    public void stuff() {
+    public void wikipediaPage() {
+        def url = "http://en.wikipedia.org/wiki/Edmund_Sharpe"
+
+        def result = doIt(url)
+
+        assert result.startsWith('<div id="mw-content-text" lang="en" dir="ltr" class="mw-content-ltr">')
+    }
+
+
+    private doIt(url) {
         String boilerpipe = ArticleSentencesExtractor.INSTANCE.getText(new URL(url))
         Document document = Jsoup.connect(url).get()
-        String startingText = boilerpipe.split("\n")[0]
-        println document.body().getElementsContainingOwnText(startingText).parents()[0]
+
+        def elements = null
+        for (String startingText: boilerpipe.split("\n")) {
+            elements = document.body().getElementsContainingOwnText(startingText.take(25)).parents()[0]
+            if (elements != null) {
+                break;
+            }
+        }
+        elements.toString()
     }
 }
